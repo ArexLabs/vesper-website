9747c5823f2a8a239bc306ef2aa42fe43808d39d
ItzzMateo <itzzmateo@devflare.de>
2026-05-26 19:53:08 +0200

    chore(deps): update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -17,7 +16,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  /* config options here */
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
